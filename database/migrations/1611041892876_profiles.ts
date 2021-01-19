0e307c219d0dd65e9180897ab0016e78c5e18246
Mrinal Kumar Singh <mrinal.singh@mountblue.tech>
2021-01-19 20:01:00 +0530

    Defined the foreign key as unsigned.

--- database/migrations/1611041892876_profiles.ts
+++ database/migrations/1611041892876_profiles.ts
@@ -7,6 +7,7 @@ export default class Profiles extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.integer('user_id')
+           .unsigned()
            .references('id')
            .inTable('users')
       table.integer('age').notNullable()
